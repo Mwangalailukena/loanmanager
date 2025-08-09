0267e1b0bb1929d6a64342d6c3413cdbeb99137a
Mwangala <ilukenamwangala@gmail.com>
2025-08-09 22:42:39 +0200

    update

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -19,7 +19,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Check if service worker still exists or not.
