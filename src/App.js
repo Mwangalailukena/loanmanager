cff3d04e5896a6a3a1f48f8f8746932fc203a089
Mwangalailukena <ilukenamwangala@gmail.com>
2025-08-23 18:26:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,55 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import AppThemeProvider from './contexts/ThemeProvider.jsx';
 import { useThemeContext } from './contexts/ThemeProvider.jsx';
+import { AuthProvider, useAuth } from './contexts/AuthProvider';
 import { FirestoreProvider } from './contexts/FirestoreProvider';
-import { AuthProvider } from './contexts/AuthProvider';
 
 import AppRoutes from './AppRoutes';
 import InstallPrompt from './components/InstallPrompt';
 import SplashScreen from './components/SplashScreen';
+import NetworkStatus from './components/NetworkStatus';
 
 import { ToastContainer } from 'react-toastify';
-
-import NetworkStatus from './components/NetworkStatus';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const { darkMode, onToggleDarkMode } = useThemeContext();
-  const [showSplash, setShowSplash] = useState(true);
+  const { loading } = useAuth(); // Get the loading state from the AuthProvider
 
-  useEffect(() => {
-    const splashTimeout = setTimeout(() => {
-      setShowSplash(false);
-    }, 2000);
-
-    return () => clearTimeout(splashTimeout);
-  }, []);
-
-  if (showSplash) {
+  // Conditional rendering based on the loading state
+  if (loading) {
     return <SplashScreen />;
   }
 
   return (
     <Router>
-      <AuthProvider>
-        <FirestoreProvider>
-          <NetworkStatus />
-          <AppRoutes darkMode={darkMode} onToggleDarkMode={onToggleDarkMode} />
-          <InstallPrompt />
-          
-          {/* Keep the ToastContainer, but remove the custom props */}
-          <ToastContainer position="top-center" />
-        </FirestoreProvider>
-      </AuthProvider>
+      <FirestoreProvider>
+        <NetworkStatus />
+        <AppRoutes darkMode={darkMode} onToggleDarkMode={onToggleDarkMode} />
+        <InstallPrompt />
+        <ToastContainer position="top-center" />
+      </FirestoreProvider>
     </Router>
   );
+  
 }
 
-// Wrap the App component with the AppThemeProvider at the top level
+// Wrap the App component with the necessary providers at the top level
 const AppWithProviders = () => (
   <AppThemeProvider>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </AppThemeProvider>
 );
 
