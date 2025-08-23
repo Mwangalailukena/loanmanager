0bf72747352440b17024deefd16df42d0288120f
Mwangala <ilukenamwangala@gmail.com>
2025-08-24 00:10:52 +0200

    fix: change service worker generation to fix module error

--- workbox-config.js
+++ workbox-config.js
@@ -2,7 +2,6 @@
 module.exports = {
   globDirectory: 'build/',
   globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-  swSrc: 'src/sw.js',
   swDest: 'build/sw.js',
-  injectionPoint: 'self.__WB_MANIFEST',
+  importScripts: ['src/sw.js'],
 };
