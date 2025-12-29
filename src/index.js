3fd447dca5f704a0a0c0f632820299dbdc4888bc
Mwangalailukena <ilukenamwangala@gmail.com>
2025-12-29 21:21:50 +0200

    revert: Remove OneSignal integration and restore service workers
    Reverted all changes related to OneSignal integration, including:
    - Removing OneSignal initialization from src/index.js.
    - Removing OneSignal SDK script from public/index.html.
    - Removing OneSignal import from src/custom-sw.js.
    - Restoring the build script in package.json.
    - Removing OneSignalSDKWorker.js from .gitignore.
    
    The application's original service worker setup is now restored.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import AppThemeProvider from './contexts/ThemeProvider';
 import { SnackbarProvider } from './components/SnackbarProvider';
-
-window.OneSignalDeferred = window.OneSignalDeferred || [];
-window.OneSignalDeferred.push(async function(OneSignal) {
-  await OneSignal.init({
-    appId: "85fb7b5e-ca58-4a5a-9223-6477f32c4992",
-    safari_web_id: "web.onesignal.auto.028d9952-ba2c-477b-babc-6aee5c5ba0de",
-    notifyButton: {
-      enable: true,
-    },
-  });
-});
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -27,4 +17,4 @@ root.render(
   </React.StrictMode>
 );
 
-// serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
