0c176c13408b1ec558c24c85173721a2dd9b123e
Mwangalailukena <ilukenamwangala@gmail.com>
2025-12-29 19:14:29 +0200

    feat: Integrate OneSignal for push notifications
    This commit integrates OneSignal into the application for push notification
    functionality.
    
    Changes include:
    - Added OneSignal SDK script to public/index.html.
    - Integrated OneSignal service worker into public/service-worker.js.
    - Initialized OneSignal SDK in src/index.js with the provided App ID.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import AppThemeProvider from './contexts/ThemeProvider';
 import { SnackbarProvider } from './components/SnackbarProvider';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+window.OneSignal = window.OneSignal || [];
+const OneSignal = window.OneSignal;
+OneSignal.push(function() {
+  OneSignal.init({
+    appId: "85fb7b5e-ca58-4a5a-9223-6477f32c4992",
+  });
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
