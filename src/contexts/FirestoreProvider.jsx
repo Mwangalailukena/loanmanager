07c91175f4759f7aea67a6d1bc5af78489c69a02
Mwangalailukena <ilukenamwangala@gmail.com>
2026-01-01 09:19:56 +0200

    Fix: suppress unused variable warning in FirestoreProvider.jsx to resolve ESLint build error

--- src/contexts/FirestoreProvider.jsx
+++ src/contexts/FirestoreProvider.jsx
@@ -308,6 +308,7 @@ export function FirestoreProvider({ children }) {
           const loanData = loanSnap.data();
           const currentPrincipal = Number(loanData.principal || 0);
           const currentInterestDuration = Number(loanData.interestDuration || 1);
+          // eslint-disable-next-line no-unused-vars
           const currentRepaidAmount = Number(loanData.repaidAmount || 0);
 
           const newPrincipal = currentPrincipal + topUpAmount;
